24cc56d3c87cac16e7bfa9a5f96c21b781e0604b
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2021-07-09 16:01:57 -0400

    initial

---
+++ data.js
@@ -0,0 +1,224 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-wellmore-7",
+      "name": "Wellmore #7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.0913296798673926,
+        "pitch": 0.2288402749935905,
+        "fov": 1.240707871342459
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8131271962539017,
+          "pitch": 0.021839403455960138,
+          "rotation": 0,
+          "target": "1-wellmore-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4221665677457196,
+          "pitch": -0.06806386057555969,
+          "title": "Harper's Branch",
+          "text": "Impoundment Fill"
+        },
+        {
+          "yaw": 0.8599663201908143,
+          "pitch": 0.5063482171450069,
+          "title": "Office",
+          "text": "Training Center"
+        },
+        {
+          "yaw": -0.5806211217800978,
+          "pitch": 0.18317767438931476,
+          "title": "Stockpiles",
+          "text": "71 - 76"
+        }
+      ]
+    },
+    {
+      "id": "1-wellmore-8",
+      "name": "Wellmore #8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.14332748899227177,
+        "pitch": 0.654821260897462,
+        "fov": 1.2390811135500592
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3337818630919465,
+          "pitch": -0.028542993264519367,
+          "rotation": 0,
+          "target": "2-wellmore-14"
+        },
+        {
+          "yaw": 2.941292665334312,
+          "pitch": 0.03647281116343315,
+          "rotation": 0,
+          "target": "0-wellmore-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8053948799570101,
+          "pitch": 0.792448487603771,
+          "title": "Stockpile",
+          "text": "87"
+        },
+        {
+          "yaw": -1.1697722832985598,
+          "pitch": 0.32275505674527594,
+          "title": "Stockpile",
+          "text": "88"
+        },
+        {
+          "yaw": -0.8264860139423256,
+          "pitch": 0.15445532504780246,
+          "title": "Stockpiles",
+          "text": "82 and 83"
+        },
+        {
+          "yaw": 0.737288761818574,
+          "pitch": -0.055289564499416244,
+          "title": "Stockpile",
+          "text": "86"
+        },
+        {
+          "yaw": -0.5982021692049742,
+          "pitch": -0.05326897180350976,
+          "title": "Stockpiles",
+          "text": "84 and 85"
+        }
+      ]
+    },
+    {
+      "id": "2-wellmore-14",
+      "name": "Wellmore #14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 1.1438623025632317,
+        "pitch": 0.5583089446605083,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9941795131838447,
+          "pitch": 0.02132715088802506,
+          "rotation": 0,
+          "target": "1-wellmore-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8477024058079374,
+          "pitch": 0.44787755677945285,
+          "title": "Shop Buildings",
+          "text": "Lab currently inside"
+        },
+        {
+          "yaw": 0.7386362935931796,
+          "pitch": 0.6286249236977426,
+          "title": "Stockpiles",
+          "text": "14A and 14B"
+        },
+        {
+          "yaw": -1.883757482635115,
+          "pitch": 0.6946788047352221,
+          "title": "Stockpile",
+          "text": "14E"
+        }
+      ]
+    }
+  ],
+  "name": "Wellmore Prep Plant: Big Rock, VA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
